73459bb478d9cbab73cf082e931f93620f4f26a2
zhuravelta963-lang <zhuravelta963@gmail.com>
2026-05-07 10:17:47 +0300

    Add files via upload

---
+++ CW_less1/script/js.js
@@ -0,0 +1,74 @@
+// console.log("Hell JS"); //службові повідомлення для розробника
+// console.info("hell JS #2") //різниці з попереднім немає
+// console.error("Hell JS #3"); //так позначають помилку
+// console.warn("Hell JS #4"); //звернути увагу
+
+
+// var myFirstVar;
+// myFirstVar = 10;
+// console.log(myFirstVar);
+//
+// var mySecondVar = '2';
+// console.log("Змінна: " + mySecondVar);
+// console.log(mySecondVar + myFirstVar);
+// console.log(myFirstVar - mySecondVar);
+
+// var first, second = 2, "4";
+
+
+//типи даних
+// var int = 10;
+// var float = 10.2;
+// var bool = true; //false
+// var str = 'str';
+// var nothing;
+// nothing = null;
+
+// num = 2;
+// var num;
+// console.log(num);
+
+
+// let num= 13;
+// num = '14';
+// console.log(num);
+//
+// const pi = 3.14; //константам не можна змінювати значення
+
+//математичні оператори (8) - команда, яка створює взаємодію
+// let a = 21, b = 4;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// let a, b;
+// a = 2 + 4;
+// b = 3 * (a = 10 + 3);
+
+// a = a + 1;
+// a += 1;
+//
+// a++; //інкремент, суфіксна форма
+// a--; //декремент
+//
+// ++a; //префіксна форма
+// --a;
+
+// let a = 2, b;
+// b = a++;
+// b = ++a;
+// console.log(a);
+
+// let a = '2', b = '3';
+// console.log((a - 0) + (b - 0));
+// console.log(Number(a) + Number(b));
+
+//інтерполяція (конкатинація), щеплення
+let name = "Tatiana";
+console.log("Привіт " + name + "!");
+let skill = "WEB";
+
+console.log(`Привіт ${name}! Твій найкращий скіл - це ${skill} ${2 + 4}`);
